dc1debc3be168d939134eeca9077eaf2dff0495f
KampanyaTakip <user@kampanyatakip.app>
2026-06-13 01:27:30 +0300

    fix: Ekle butonuna mobil uyumluluk icin inline reset stilleri uygulandi, surum v27'ye yukseltildi

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = {
   batchMode: false,
   selectedCampaigns: new Set(),
   config: {
-    version: '1.0.0 (v26)',
+    version: '1.0.0 (v27)',
     demandFormUrl: 'https://corewishasset.com.tr/digital-form/demand-form/create/75'
   },
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kampanya-takip-v26';
+const CACHE_NAME = 'kampanya-takip-v27';
 const ASSETS = [
   './',
   './index.html',
